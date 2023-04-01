744a38170c1c0dec98468794b56e6e911012344d
Nathan Tablang <hans@Nathans-MacBook-Air-5.local>
2023-04-01 03:55:38 -0400

    more

---
+++ frontend/components/Subsections/Dashboard/APIPane.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function APIPane() {
+    
+
+
+  return (
+    <div className='w-full h-screen border p-12'>
+        <div className='text-[#808080a4] font-inter font-light mb-10'>Connected APIs</div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default APIPane
---
+++ frontend/components/Subsections/Dashboard/Content.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <div className='w-full h-screen p-10'>Content</div>
+  )
+}
+
+export default Content
