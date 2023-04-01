6477df3a3c2ad0e415e99585a726ec668135818a
Nathan Tablang <hans@Nathans-MacBook-Air-5.local>
2023-04-01 03:42:01 -0400

    more

---
+++ frontend/pages/dashboard.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/Sidebar";
+import React from "react";
+
+function Dashboard() {
+  return (
+    <>
+      <Sidebar />
+      <div>dashboard</div>
+    </>
+  );
+}
+
+export default Dashboard;
