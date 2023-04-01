3f7298fecb58e20c39f86c4ae853cc0c8ccaad10
Richard McHorgh <mchorghr2@gmail.com>
2023-03-31 23:45:25 -0400

    added types

--- scheduler.ts
+++ scheduler.ts
@@ -1,1 +1,43 @@
-// hello 
+const onSchedule = (s, r) => {
+    console.log(s, r)
+}
+
+let exports: any = {}
+
+import type {User, Period} from 'types.d'
+
+// Schedule function to run on a weekly interval.
+
+exports.scheduleWeekly = onSchedule(
+    'every week 00:00',
+    async (ev) => {
+        // get users with weekly schedule task
+        const weeklyUsers = await getUsersByPeriod(Period.weekly)
+
+        // run job on each user
+        weeklyUsers.forEach(runJob)
+    }
+)
+
+
+const runJob = (user: User) => {
+    // get user template
+
+    // run the llm on their data
+
+    // send email
+}
+
+const getUsersByPeriod = async (period: Period): Promise<User[]> => {
+    const ref = collection(db, 'users')
+    const q = query(ref, where('period', '==', period))
+
+    let collection = []
+
+    const snap = await getDocs(q)
+    snap.forEach(doc => {
+        collection.push(doc.data())
+    })
+
+    return collection
+}
