22202f395fb26eba6a8a5ec7cc6bed46fd5544f4
littleTitan <luke.ballmer@outlook.com>
2023-03-31 23:36:38 -0400

    backend template

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+# Text linear summary (text_linear_summary.js)
+Use GPT to summarize text titles of different issues.
+
---
+++ backend/index.js
@@ -0,0 +1,5 @@
+import { module as tlinear_mod } from "./text_linear_summary.js";
+
+tlinear_mod("Tell me about yourself.")
+
+
